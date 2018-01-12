cf40ab39b950d1cdaa754f6b54f505caff717dce
Nick Santamaria <nick.santamaria@previousnext.com.au>
2018-01-12 17:49:51 +1100

    Move version constants into separate package.

--- scripts/generate-version.go
+++ scripts/generate-version.go
@@ -9,9 +9,13 @@ import (
 	"github.com/pkg/errors"
 )
 
+const (
+	versionFile = "version/version.go"
+)
+
 func main() {
-	out, _ := os.Create("version.go")
-	out.Write([]byte("package main \n\nconst (\n"))
+	out, _ := os.Create(versionFile)
+	out.Write([]byte("package version \n\nconst (\n"))
 
 	// Execute git describe to insert the version identifier.
 	version, err := execGitDescribe()
